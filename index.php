c68d005bbe819b2462b5d89fc86ab4f5623883c0
balezeauquentin <94165700+balezeauquentin@users.noreply.github.com>
2022-10-05 20:31:59 +0200

    Metadonnee

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-SALUT
+<html lang ="fr">
+<head>
+    <meta charset ="UTF-8">
+    <meta name = "author" content="Quentin,Eloi,William">
+    <meta name ="description" content="This is a page about beer">
+    <link rel="shortcut icon" href="logo.jpg" type="image/x-icon">
+    <link rel="stylesheet" href="page.css">
+    <title>Beer advisor</title>
+</head>
+<body>
+
+</body>
+</html>
