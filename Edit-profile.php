4a7e679d6f1ba4f6cfe94ff6d982dd7c9261f040
Wiqiro <william.imbert@utbm.fr>
2022-11-11 10:12:37 +0100

    Photo de profile

--- Edit-profile.php
+++ Edit-profile.php
@@ -3,7 +3,6 @@ session_start();
 require_once("connection.php");
 global $bdd;
 $id = $_SESSION['ID'];
-echo $id;
 $req = $bdd->prepare("SELECT * FROM user WHERE id = ?");
 $req->execute(array($id));
 $res = $req->fetch();
@@ -12,9 +11,22 @@ $bio = $res['Bio'];
 if(isset($_POST['confirm'])){
         $new_username = htmlspecialchars($_POST['username']);
         $new_bio = htmlspecialchars($_POST['bio']);
-        $new_req = $bdd->prepare("UPDATE user SET Username = ?, Bio = ? WHERE id = ?");
-        $new_req->execute(array($new_username,$new_bio,$id));
-        header("Location:profile.php?id=".$_SESSION['ID']);
+        
+        if (isset($_FILES['image']['tmp_name']) && $_FILES['image']['tmp_name'] != '') {
+            $size = filesize($_FILES['image']['tmp_name']);
+            if ($size < 1000000) {
+                $new_image = file_get_contents($_FILES['image']['tmp_name']);
+            }
+        }
+        if (!isset($size)) {
+            $new_req = $bdd->prepare("UPDATE user SET Username = ?, Bio = ? WHERE id = ?");
+            $new_req->execute(array($new_username,$new_bio,$id));
+            header("Location:profile.php?id=".$_SESSION['ID']);
+        } else if ($size < 1000000) {
+            $new_req = $bdd->prepare("UPDATE user SET Username = ?, Bio = ?, Picture = ? WHERE id = ?");
+            $new_req->execute(array($new_username, $new_bio, $new_image, $id));
+            header("Location:profile.php?id=".$_SESSION['ID']);
+        }
 }
 
 ?>
@@ -29,7 +41,7 @@ if(isset($_POST['confirm'])){
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <form action="" method="post">
+    <form action="" method="post" enctype="multipart/form-data">
         <div class="container">
             <hr>
             <table>
@@ -38,7 +50,13 @@ if(isset($_POST['confirm'])){
                         <label for="username"></label>
                         <input type="text" name="username" id="username" placeholder="Username" value="<?php echo $username ?>" required>
                         <label for="bio"></label>
-                        <input type="text" name="bio" id="bio" placeholder="bio" value="<?php echo $bio ?>"
+                        <input type="text" name="bio" id="bio" placeholder="bio" value="<?php echo $bio ?>">
+                        <input type="file" name="image" accept=".jpg, .jpeg, .png">
+                        <?php 
+                        if (isset($size) && $size >= 1000000) {
+                            echo "The maximum upload size is 1 mb";
+                        }
+                        ?>
                     </td>
                 </tr>
             </table>
--- profile.php
+++ profile.php
@@ -7,10 +7,10 @@ if (isset($_GET['id']) and $_GET['id'] > 0) {
     $profileid = intval($_GET['id']);
     $req_profile = $bdd->prepare("SELECT * FROM user WHERE id = ?");
     $req_profile->execute(array($_GET['id']));
-    $res = $req_profile->fetch();
-    $username = $res['Username'];
-    $date = $res['Creation_date'];
-    $bio = $res['Bio'];
+    $profile_data = $req_profile->fetch();
+    $username = $profile_data['Username'];
+    $date = $profile_data['Creation_date'];
+    $bio = $profile_data['Bio'];
     $req_friend = $bdd->prepare("SELECT COUNT(*) FROM follows WHERE Follower_ID = ? AND Followed_ID = ?");
     $req_friend->execute(array($id, $profileid));
     $result_friend = $req_friend->fetch();
@@ -29,39 +29,45 @@ if (isset($_GET['id']) and $_GET['id'] > 0) {
 ?>
 
 
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <title>Beer advisor</title>
-    <meta name="author" content="Quentin,Eloi,William">
-    <meta name="description" content="This is a page about beer">
-    <link rel="shortcut icon" href="logo.jpg" type="image/x-icon">
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-<p class="logo"><a href="index.php"><img src="BeerAdvisor.png"></a></p>
-<div>c'est le profil de <?php
-    echo $username;
-    echo '<br>';
-    echo $date;
-    echo '<br>bio<br>';
-    echo $bio;
-    echo '<br><href="Comment'
-    ?>
-    <br><?php
-    if ($id == $profileid) {
-        echo '<a href="Edit-profile.php">Edit profile</a>';
-    } else if (isset($message)) {
-        echo $message;
-    } else {
-        echo '<form method="post">
-                    <label for="Add_friend">Add Friend</label>
-                    <input type="submit" id="Add_friend" name="Add_friend" Value="Add friend">
-              </form>';
-    }
-    ?>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Beer advisor</title>
+        <meta name="author" content="Quentin,Eloi,William">
+        <meta name="description" content="This is a page about beer">
+        <link rel="shortcut icon" href="logo.jpg" type="image/x-icon">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <p class="logo"><a href="index.php"><img src="BeerAdvisor.png"></a></p>
+        <div>
+            <table><tr>
+                <td>
+					<div><?php echo '<img src="data:image;base64,' . base64_encode($profile_data["Picture"]) . '" alt=""/>'; ?></div>
+                </td>
+                <td>
+                Profile: <?php
+                echo $username . '<br>
+                Member since: ' . $date . '<br>Bio: <br>' .
+                $bio . '<br>'
+                ?><br>
+                </td>
+            </tr></table>
+            
+            <?php
+            if ($id == $profileid) {
+                echo '<a href="Edit-profile.php">Edit profile</a>';
+            } else if (isset($message)) {
+                echo $message;
+            } else {
+                echo '<form method="post">
+                            <label for="Add_friend">Add Friend</label>
+                            <input type="submit" id="Add_friend" name="Add_friend" Value="Add friend">
+                    </form>';
+            }
+            ?>
 
 
-</div>
-</body>
+        </div>
+    </body>
 </html>
